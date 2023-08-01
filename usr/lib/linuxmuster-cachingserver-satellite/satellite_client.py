2a981afa43b82aa06e52588eab1990b439f6fe6d
Lukas Spitznagel <lukas.spitznagel@netzint.de>
2023-08-01 10:04:27 +0000

    Rewrite server and client

--- usr/lib/linuxmuster-cachingserver-satellite/satellite_client.py
+++ usr/lib/linuxmuster-cachingserver-satellite/satellite_client.py
@@ -9,170 +9,122 @@
 
 import socket
 import logging
-import json
+import hashlib
 import os
-import argparse
-import subprocess
+import threading
+import time
+import json
 
-from subprocess import PIPE
-
-logging.basicConfig(filename='/var/log/linuxmuster/cachingserver/satellite.log',format='%(levelname)s: %(asctime)s %(message)s', level=logging.DEBUG)
-
-def error(msg):
-    logging.error(msg)
-    exit(1)
-
-def __execute(command):
-    return subprocess.run(command, stdout=PIPE, stderr=PIPE)
-
-def sendMessage(conn, msg):
-    logging.debug(f"Send message '{msg}'")
-    conn.send(msg.encode("utf-8"))
-
-def receiveMessage(conn):
-    message = conn.recv(1024).decode()
-    if len(message) > 40:
-        logging.debug("Receive message '" + message[:40].replace('\n', '') + "...'")
-    else:
-        logging.debug(f"Receive message '{message}'")
-    return message
-
-def receiveData(conn, part=None):
-    data = conn.recv(1024)
-    if part != None:
-        logging.debug(f"[{part}] Receive data...")
-    else:
-        logging.debug("Receive data...")
-    return data
+logging.basicConfig(filename='/var/log/linuxmuster/cachingserver/client.log',format='%(levelname)s: %(asctime)s %(message)s', level=logging.DEBUG)
 
 def getSatelliteConfig():
     return json.load(open("/var/lib/linuxmuster-cachingserver/server.json", "r"))
 
-def connect(ip, port):
-    logging.info(f"Starting new connection to {ip} on port {port}...")
-    try:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((ip, port))
-        logging.info("Connection successful!")
-        return client
-    except Exception as e:
-        logging.error(f"Connection to {ip} on port {port} not possible! Message: " + str(e))
-        return False
+def getMD5Hash(filename):
+    md5_hash = hashlib.md5()
+    with open(filename,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+            md5_hash.update(byte_block)
+    return md5_hash.hexdigest()
 
-def authenticate(client, config):
-    sendMessage(client, "auth %s" % config["key"])
-    result = receiveMessage(client).split(" ", 1)
+def send(client, message):
+    client.send(message.encode())
 
-    if result[0] != "hello":
-        logging.error(f"Authentification failed! Connection to {client.getpeername()[0]} is not possible!")
-        return False
+def receive(client):
+    return client.recv(1024).decode()
 
-    logging.info("Authentification successful!")
-    return True
-    
-def get(client, actions):
-    sendMessage(client, "get " + actions)
-    if receiveMessage(client) != "ok":
-        logging.error("Server said action is invalid!")
-        return False
-
-    sendMessage(client, "prehook?")
-    prehook = receiveMessage(client)
-    if prehook != "no":
-        logging.info(f"Running prehook: {prehook}")
-        hookResult = __execute(prehook.split(" "))
-        logging.info(f"Result: {hookResult}")
-    sendMessage(client, "done")
-
-    sendMessage(client, "start")
-    invalidtaskcount = 0
+def printFileTransferStatus(filename, filesize):
     while True:
-        task = receiveMessage(client)
-        logging.debug(f"Receive task {task}")
-        if task == "end":
-            logging.info("All files are received successfully!")
+        time.sleep(5)
+        currentFilesize = os.stat(filename).st_size
+        percent = round((currentFilesize / filesize) * 100, 2)
+        if currentFilesize == filesize:
             break
-        elif task == "file":
-            invalidtaskcount = 0
-            sendMessage(client, "ok")
-            headers = receiveMessage(client)
-            filename = headers.split(";")[0]
-            size = int(headers.split(";")[1])
-            logging.info(f"Receiving file {filename} with {size} bytes")
-            sendMessage(client, "ok")
-            path = "/".join(filename.split('/')[0:-1])
-            if not os.path.exists(path):
-                os.makedirs(path)
-            if size > 0:
-                file = open(filename, "wb")
-                fileparts = (int(size / 1024) + 1)
-                logging.debug(f"Receiving file in {fileparts} parts...")
-                for i in range(0, (fileparts)):
-                    file.write(receiveData(client, (i + 1)))
-            else:
-                logging.debug("File is empty. Only generating empty file!")
-                with open(filename, "w") as file:
-                    file.write("")
-            sendMessage(client, "ok")
-            logging.info(f"File {filename} received successfully!")
-        else:
-            invalidtaskcount += 1
-        
-        if(invalidtaskcount > 10):
-            logging.error("To many invalid messages. Maybe server is not responding anymore...?")
-            return False
-
-    sendMessage(client, "posthook?")
-    posthook = receiveMessage(client)
-    if posthook != "no":
-        logging.info(f"Running posthook: {posthook}")
-        hookResult = __execute(posthook.split(" "))
-        logging.info(f"Result: {hookResult}")
-    sendMessage(client, "done")
-    return True
-
-def api(items):
-    config = getSatelliteConfig()
-    server = config["server_name"] + "." + config["server_domain"]
-    port = config["server_port"]
-    client = connect(server, port)
-    if client:
-        if authenticate(client, config):
-            if "," in items:
-                for item in items.split(","):
-                    get(client, item)
-            else:
-                get(client, items)
-            sendMessage(client, "bye")
-            logging.info("Finished!")
-            client.close()
-        else:
-            return False
-    return False
+        logging.info(f"Transfered {percent}% of '{filename}' ({currentFilesize}/{filesize})")
 
 def main():
     config = getSatelliteConfig()
-    server = config["server_name"] + "." + config["server_domain"]
+    host = config["server_name"] + "." + config["server_domain"]
     port = config["server_port"]
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--items", nargs="+", required=True, help="List items to sync")
-    args = parser.parse_args()
 
-    client = connect(server, port)
-    if client:
-        if authenticate(client, config):
-            for item in args.items:
-                if not get(client, item):
-                    logging.error("Error processing %s. Continue with next one..." % item)
-            sendMessage(client, "bye")
-            logging.info("Finished!")
-            client.close()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    send(client, "auth " + config["key"])
+    if receive(client) != "ok":
+        logging.error("No valid answer!")
+
+    send(client, "get images")
+    finished = False
+    while not finished:
+        data = receive(client).split(" ") # get filename and size
+        if data[0] == "finished":
+            finished = True
+            logging.info(f"All files transfered!")
+            send(client, "ok")
+            break
+        if data[0] != "send":
+            logging.error("No valid answer!")
+            break
+        filename = data[1]
+        filesize = int(data[2])
+        counter = 0
+        errorcounter = 0
+        logging.info(f"Receive new file '{filename}'...")
+        send(client, "ok")
+        if not os.path.exists(os.path.split(filename)[0]):
+            logging.info(f"Path '{os.path.split(filename)[0]}' does not exist. Create it...")
+            os.makedirs(os.path.split(filename)[0], exist_ok=True)
+        f = open(filename, "wb")
+        if filesize == 0:
+            f.write(b"")
         else:
-            exit(1)
+            statusThread = threading.Thread(target=printFileTransferStatus, args=(filename, filesize))
+            statusThread.daemon = True
+            statusThread.start()
+            while True:
+                data = client.recv(1024)
+                f.write(data)
+                counter += len(data)
+                if len(data) == 0:
+                    errorcounter += 1
+                if counter == filesize:
+                    break
+                if errorcounter > 10:
+                    logging.error(f"Error while receiving file {filename}")
+                    break
+                
+        f.close()
+        logging.info(f"File '{filename}' received. Checking file...")
+        send(client, "ok")
+        data = receive(client).split(" ") # get check command
+        if data[0] != "check":
+            logging.error("No valid answer!")
+            break
+        send(client, getMD5Hash(filename))
+        check = receive(client)
+        if check == "restart":
+            logging.info(f"File '{filename}' is invalid. Download will be retried...")
+            continue
+        if check != "success":
+            logging.error("No valid answer!")
+            break
+        logging.info(f"File '{filename}' is valid!")
+        send(client, "ok")
+
+    if receive(client) != "bye":
+        logging.error("Server does not say bye... Now i'm sad....")
     else:
-        exit(1)
+        logging.info(f"Server said bye. Terminate connection...")
+
+    client.close()
+
     
 
 if __name__ == "__main__":
-    main()
-
+    logging.info("======= STARTED =======")
+    try:
+        main()
+    finally:
+        logging.info("======= FINISHED =======")
+        
