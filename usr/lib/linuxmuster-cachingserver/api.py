3249beb9233cb8cf8ba0687da7a3413ec163bf99
hermanntoast <lukas.spitznagel@netzint.de>
2023-09-19 13:44:55 +0200

    Update

--- usr/lib/linuxmuster-cachingserver/api.py
+++ usr/lib/linuxmuster-cachingserver/api.py
@@ -25,7 +25,11 @@ app = FastAPI()
 
 @app.get("/status")
 def get_status_of_cachingserver():
-    return { "status": True, "data": None }
+    return { "status": True, "data": "Cachingserver is reachable!" }
+
+#########################################
+# FILES
+#########################################
 
 @app.get("/files/hashes/get")
 def get_filehashes_from_cachingserver():
@@ -33,21 +37,51 @@ def get_filehashes_from_cachingserver():
         return { "status": True, "data": json.load(f) }
     
 @app.get("/files/hashes/regenerate")
-def regenerate_filehashes_on_cachingserver():
-    subprocess.Popen(["/usr/lib/linuxmuster-cachingserver/cacheFileHashes.py"])
-    return { "status": True, "data": None }
+def regenerate_filehashes_on_cachingserver(background_tasks: BackgroundTasks):
+    def initCache():
+        subprocess.Popen(["/usr/lib/linuxmuster-cachingserver/cacheFileHashes.py"])
+    background_tasks.add_task(initCache)
+    return { "status": True, "data": "Successful initiated!" }
 
 @app.get("/files/sync/{item}")
 def sync_files_from_server_to_cachingserver(item, background_tasks: BackgroundTasks):
     def initSync():
         client = SocketHepler(config["server_ip"], config["server_port"]).connect(config["secret"])
         client.sync(item)
-    try:
-        background_tasks.add_task(initSync)
-        threading.Thread(target=initSync()).start()
-        return { "status": True, "data": None }
-    except Exception as e:
-        return { "status": False, "data": str(e) }
+    background_tasks.add_task(initSync)
+    return { "status": True, "data": "Successful initiated!" }
+
+#########################################
+# SERVICES
+#########################################
+
+@app.get("/services/status/{service}")
+def get_status_of_service(service):
+    stat = subprocess.call(["systemctl", "is-active", "--quiet", service])
+    if(stat == 0):
+        return { "status": True, "data": "Service is active" }
+    return { "status": False, "data": "Service is unknown or inactive" }
+
+@app.get("/services/start/{service}")
+def start_given_service(service, background_tasks: BackgroundTasks):
+    def start():
+        subprocess.Popen(["/usr/bin/systemctl", "start", service])
+    background_tasks.add_task(start)
+    return { "status": True, "data": "Initiate start successfully!" }
+
+@app.get("/services/stop/{service}")
+def stop_given_service(service, background_tasks: BackgroundTasks):
+    def stop():
+        subprocess.Popen(["/usr/bin/systemctl", "stop", service])
+    background_tasks.add_task(stop)
+    return { "status": True, "data": "Initiate stop successfully!" }
+
+@app.get("/services/restart/{service}")
+def restart_given_service(service, background_tasks: BackgroundTasks):
+    def restart():
+        subprocess.Popen(["/usr/bin/systemctl", "sretart", service])
+    background_tasks.add_task(restart)
+    return { "status": True, "data": "Initiate restart successfully!" }
 
 def main():
     uvicorn.run(app, host="0.0.0.0", port=4457)
