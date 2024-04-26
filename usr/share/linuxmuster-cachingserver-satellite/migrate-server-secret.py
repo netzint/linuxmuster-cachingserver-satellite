de5efe9e1c1465894ca1a564d572e2d1f560f717
hermanntoast <lukas.spitznagel@netzint.de>
2024-04-26 14:13:35 +0000

    Fix some problems

--- usr/share/linuxmuster-cachingserver-satellite/migrate-server-secret.py
+++ usr/share/linuxmuster-cachingserver-satellite/migrate-server-secret.py
@@ -9,7 +9,8 @@ print("# Migrating server to rsync file...")
 
 with open("/var/lib/linuxmuster-cachingserver/server.json") as f:
     server = json.load(f)
-    print(f"# Add secret for {server['name']} to rsync file...", end="")
-    with open("/var/lib/linuxmuster-cachingserver/cachingserver_rsync.secret", "w") as f2:
-        f2.write(server["secret"])
-    print(" ok")
+    if "name" in server:
+        print(f"# Add secret for {server['name']} to rsync file...", end="")
+        with open("/var/lib/linuxmuster-cachingserver/cachingserver_rsync.secret", "w") as f2:
+            f2.write(server["secret"])
+        print(" ok")
